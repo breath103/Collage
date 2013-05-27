9f122e6697e6580c9755beb37b41779b2fd5833b
breath103 <breath103@gmail.com>
2013-05-27 21:10:47 +0900

    index on master: 409b312 delete

--- routes/site/index.js
+++ routes/site/index.js
@@ -10,6 +10,7 @@ module.exports = function(app){
 	
 	var Collage = mongoose.models.Collage;
 	var User    = mongoose.models.User;
+
 	app.get("/collages",function(req,res){
         Collage.find({},function(err,users){
             if(err){
@@ -19,6 +20,7 @@ module.exports = function(app){
             }
         });
 	});
+	
 	
 	app.get("/collages/:id",function(req,res){
 		Collage.findById(req.param("id"),function(err,collage){
@@ -30,21 +32,40 @@ module.exports = function(app){
         });
 	});
 	
-	app.get("/users/:id",function(req,res){
+	
+	app.get("/users/:id",function(req,res,next){
+		User.findById(req.param("id"),function(err,user){
+			user.getCreateCollages(function(err,collages){
+				console.log("!!" , collages);
+			});
+		});
 		User.findById(req.param("id"))
+			.populate("_create_collages")
+			.populate("_invited_collages")
 			.exec(function(err,user){
-		        Collage.find({_creator : user._id},function(err,collages){
-		            if(err){
-		                res.send(401);
-		            } else {
-						console.log(collages);
-						user = user.toJSON();
-						user.collages = collages;
-						res.render("user/index",{user : user});
-		            }
-		        });
+		     	if(err) next(err);
+				else res.render("user/index",{user : user});
+				console.log(user);
 			});
 	});
+	
+	app.get("/users/:id/new_collage",function(req,res,next){
+		User.findById(req.param("id")).exec(function(err,user){
+			if(err) next(err);
+			else {
+			    var collage = new Collage({
+					name : "new collections 2",
+					_creator : user._id 
+			    });
+			    collage.save(function (err) {
+					user._create_collages.push(collage._id);
+					user.save();
+				});
+				res.render("user/index",{user : user});
+			}
+		});
+	});
+	
 
     // require("./admin")(app);
     // require("./auth")(app);
