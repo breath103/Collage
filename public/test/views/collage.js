d3658f2e51912aa8906b221a5190e9621794188e
Sean Moon <breath103@gmail.com>
2013-05-31 15:24:36 +0900

    add sample tests (works well!)

--- public/test/views/collage.js
+++ public/test/views/collage.js
@@ -4,4 +4,23 @@ describe("CollageView",function(){
 			CollageView.should.be.an("function");
 		});
 	});
+	describe("attributes",function(){
+		this.view = null
+		beforeEach(function(){
+			this.view = new CollageView({ 
+				model : new Collage({
+					objets : {
+						images : [],
+						links : []
+					} 
+				}) 
+			});
+		});
+		it("should have instance",function(){
+			this.view.should.be.an.instanceof(CollageView);
+		});
+	});
+	describe("render",function(){
+		
+	});
 });
--- public/test/views/image_objet.js
+++ public/test/views/image_objet.js
@@ -1,8 +1,28 @@
 describe("ImageObjet",function(){
-	describe("model",function(){
+	describe("prototype",function(){
 		it("should exist",function(){
-			
 			ImageObjetView.should.be.an("function");
+		});
+	});
+	describe("instance",function(){
+		this.model = null;
+		this.view = null;
+		beforeEach(function(){
+			this.model = new ImageObjet({
+				position : {x:100,y:100},
+				size : {width:100,height:100}
+			});
+			this.view  = new ImageObjetView({
+				model : this.model
+			});
+		});
+		it("should be initialized",function(){
+			this.view.should.be.an.instanceof(ImageObjetView);
+		});
+		describe("attributes",function(){
+			it("should have template",function(){
+			
+			});
 		});
 	});
 });
